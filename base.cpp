45157c7aa0ffb6321c57ca2de1a000da2166d41d
rmedeiros23 <raphael.medeiros.vieira@usp.br>
2019-03-22 05:52:52 -0300

    Base de todos os códigos

---
+++ base.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define For(i,a,b) for(int (i)=(a);(i) < (b); ++(i))
+#define INF 0x3f3f3f3f
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define fastcin ios_base::sync_with_stdio(false);
+#define ll long long
+#define endl "\n"
+#define MAX 
+
+
+
+int main()
+{	
+	fastcin;
+	
+
+}
