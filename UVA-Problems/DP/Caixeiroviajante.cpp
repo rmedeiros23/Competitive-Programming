95b41f56d7bae6b5df0512794e3ca562ceba2aac
Raphael Medeiros Vieira <raphael.medeiros.vieira@usp.br>
2019-06-20 22:55:28 -0300

    Update 1/2 Run codes

--- UVA-Problems/DP/Caixeiroviajante.cpp
+++ UVA-Problems/DP/Caixeiroviajante.cpp
@@ -1,4 +1,7 @@
-#include <bits/stdc++.h>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 #define For(i,a,b) for(int (i)=(a);(i) < (b); ++(i))
 #define INF 0x3f3f3f3f
@@ -10,46 +13,49 @@ using namespace std;
 #define ll long long
 #define endl "\n"
 #define MAX 2050 
+int xini,yini;
 int dp[MAX][405];
 vector<pair<int,int> > v;
 int n;
 int x,y;
 int trans(pair<int,int> a)
 {
-	int val = a.fi*x + a.se;
+	int val = a.fi*y + a.se;
 	return val;
 }
 int Cdist(int a,int b)
 {
 	int x1,x2,y1,y2;
-	x1 = a/x;
-	y1 = a%x;
-	x2 = b/x;
-	y2 = b%x;
+	x1 = a/y;
+	y1 = a%y;
+	x2 = b/y;
+	y2 = b%y;
 	return abs(x1-x2) + abs(y1-y2);
 }
 int back(int vis, int last)
 {
-	cout << "vis = " << vis << endl;
+//	cout << "vis = " << vis << endl;
 	if(vis == (1 << n)-1)
-		return 0;
+		return Cdist(last,trans(mp(xini,yini)));
 	int ret = INF;
 	if(dp[vis][last]!=-1)
 		return dp[vis][last];
 	for(int i=0;i<n;i++)
 	{
-		cout << "op = " << (1 << i) << " estado = "<<  !((vis >>  i)%2) << endl;
+//		cout << "op = " << (1 << i) << " estado = "<<  !((vis >>  i)%2) << endl;
 		if((vis >> i)%2 == 0)
 		{
 			vis += (1 << i);
 			int next = trans(v[i]);
 			int dist = Cdist(last,next);
+//cout << "Estou em " << last  << " Vou para " << next << " dist = " << dist << endl;
 			ret = min(ret,back(vis,next)+ dist);
 			vis -= (1 << i);
+
 		}
 		
 	}
-	cout << "ret = " << ret << endl;
+//	cout << " ret = " << ret << endl;		
 	return dp[vis][last] = ret;
 }
 
@@ -62,7 +68,6 @@ int main()
 	{
 		v.clear();
 		cin >> x >> y;
-		int xini,yini;
 		cin >> xini >> yini;
 		cin >> n;
 		for(int i=0;i<n;i++)
@@ -74,7 +79,7 @@ int main()
 		for(int i=0;i<MAX;i++)
 			for(int j=0;j<405;j++)
 				dp[i][j] = -1;
-		cout <<	back(0,trans(mp(xini,yini))) << endl;
+		cout <<	"The shortest path has length " << back(0,trans(mp(xini,yini))) << endl;
 	}
 
 }
