da5b58fb35f826061f8da80f9dbf4151faf0a51b
Raphael Medeiros Vieira <raphael.medeiros.vieira@usp.br>
2019-08-28 19:36:58 -0300

    Problema corrigido.

--- Problems/DP/Editing_a_book.cpp
+++ Problems/DP/Editing_a_book.cpp
@@ -9,14 +9,23 @@ using namespace std;
 #define fastcin ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ll long long
 #define endl "\n"
-#define debug true
+#define debug false
 #define MAX 
-
+int limite;
 vector<int> padrao,v;
-set<vector<int> > freq;
 int resp = INF;
 int at = 0;
 int n;
+
+int geth()
+{
+	int num = 0;
+	for (int i = 0; i < n-1;++i)
+	if (v[i] + 1 != v[i + 1]) num++;
+	return num;
+}
+
+
 vector<int> corte(int l,int r,int k)
 {
 	vector<int> copia,ret;
@@ -24,7 +33,7 @@ vector<int> corte(int l,int r,int k)
 		copia.pb(v[i]);
 	for(int i = 0;i<k;i++)
 	{
-		if(i>=l and i<=r)
+        if(i>=l and i<=r)
 			continue;// parte do intervalo não copia
 		ret.pb(v[i]);
 	}
@@ -48,50 +57,51 @@ vector<int> corte(int l,int r,int k)
 	return ret;
 
 }
-void solve()
+bool solve(int at)
 {
 	if(v == padrao)
 	{
-		resp = min(resp,at);
-		return;
+		return true;
 	}
-	if(freq.count(v) == true)
-		return;
+    int h = geth();
+    if(at >= limite or at *3 + h > 3*limite)
+        return false;
 	if(debug)
 	{
 		cout << "at = " << at << endl;
 		for(int i=0;i<v.size();i++)
 			cout << v[i] << endl;
 	}
-	freq.insert(v);
 	vector<int> aux;
 	for(int i=0;i<n;i++)
 	{
-		for(int j=0;j<n;j++) // intervalo l,r 
+		for(int j=i;j<n;j++) // intervalo l,r 
 		{
-			for(int k = 0;k<=n;k++) // lugar do ctrl V 
+			for(int k = j+1;k<=n;k++) // lugar do ctrl V 
 			{
 				if(k>=i and k<=j)
 					continue; // não posso pegar pra colar em coisa que está no intervalo
 				aux = v;
-				at++;
 				v  = corte(i,j,k); // copio intervao i,j e colo na posi k;
-				solve();
-				at--;
+				if(solve(at + 1))
+                    return true;
 				v = aux;
 			}
 		}
 	}
+    return false;
 }
 int main()
 {	
 	fastcin;
 	cin >> n;
+    int cont = 0;
 	while(n!=0)
 	{
+        cont++;
+        cout << "Case " << cont << ": ";
 		v.clear();
 		padrao.clear();
-		freq.clear();
 		resp = INF;
 		for(int i=0;i<n;i++)
 		{
@@ -100,7 +110,13 @@ int main()
 			v.pb(a);
 			padrao.pb(i+1);
 		}
-		solve();
+        int resp;
+        for(limite =0;limite<n;limite++)
+            if(solve(0))        
+            {
+                resp = limite;
+                break;
+            }
 		cout << resp << endl;
 		cin >> n;
 	}
